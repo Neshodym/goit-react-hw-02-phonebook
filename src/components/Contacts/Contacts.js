bc84c3b0bdfc3fad73a0f6d8a67127f2854b3734
Neshodym <voinash@gmail.com>
2023-05-02 16:49:20 +0900

    1

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,25 @@
+import css from './Contacts.module.css';
+
+export const Contacts = ({ title, contacts, onDeleteContact }) => {
+
+
+  return (
+    <>
+      <h3 className={css.title}>{title}</h3>
+      <ul>
+        {contacts.map(({ id, name, phone }) => {
+          return (
+            <li key={id} className={css.contactBox}>
+              <p>
+                {name}: {phone}
+              </p>
+              <button onClick={() => onDeleteContact(id)} className={css.btn}>
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
