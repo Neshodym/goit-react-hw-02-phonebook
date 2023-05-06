3e7ecb160c2793dfb34c1eec6b8a63608895c82b
Neshodym <voinash@gmail.com>
2023-05-06 09:27:18 +0900

    1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-<meta http-equiv="Permissions-Policy" content="interest-cohort=()"/>
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
